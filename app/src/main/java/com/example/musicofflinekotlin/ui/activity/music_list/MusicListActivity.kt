b1933c59c03108e65c5771186d11d818e38f807e
conchonha <thaibasang8@gmail.com>
2021-04-05 16:50:07 +0700

    ui song

---
+++ app/src/main/java/com/example/musicofflinekotlin/ui/activity/music_list/MusicListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.musicofflinekotlin.ui.activity.music_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.musicofflinekotlin.R
+
+class MusicListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_list)
+    }
+}
